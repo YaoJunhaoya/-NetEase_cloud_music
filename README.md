9244f127460ab4df5d398c5a6db2c20684c074e6
姚君豪 <3342859687@qq.com>
2023-10-01 16:31:42 +0800

    完成了导航栏的响应式

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # 仿网易云音乐 Vue 3 + Vite
 
-仿网易云音乐 前端 Vue3+Vite 
+## 介绍
+
+仿网易云音乐 前端 Vue3+Vite+Router+Pinia
 
 后端使用https://neteasecloudmusicapi.js.org/#/
 
 网易云音乐 NodeJS 版 API 后端代码来自: https://github.com/Binaryify/NeteaseCloudMusicApi
+
+## 未完成
+
+1. 没有完成响应式
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,6 +17,14 @@
       <div>排行榜</div>
       <div>音乐人</div>
     </div>
+
+    <!-- 手机的栏目 -->
+    <div class="lanmu-phone">
+      <svg class="icon lanmu-icon" aria-hidden="true">
+        <use xlink:href="#icon-zhanshi"></use>
+      </svg>
+    </div>
+
     <!-- 搜索 -->
     <div class="sousuo">
       <!-- 搜索图标 -->
@@ -41,87 +49,273 @@ export default {};
 </script>
 
 <style lang="less" scoped>
-.nav {
-  height: 70px;
-  width: 100%;
-  background-color: #333;
-  display: flex;
-  //   padding: 0 50px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+@breakpoint-medium: 1200px; //网页样式会变了
+@breakpoint-small: 900px; //手机大小
 
-  //   position: fixed;
-  //   top: 0;
-  //   left: 0;
-
-  .music-iocn {
+// 大屏幕
+@media (min-width: @breakpoint-medium) {
+  .nav {
+    height: 70px;
+    width: 100%;
+    background-color: #333;
+    display: flex;
+    //   padding: 0 50px;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     align-items: center;
-    justify-content: space-around;
-    background-color: transparent;
-    color: white;
-    width: 150px;
-    font-size: 20px;
-    padding: 0 50px 0 50px;
+    .music-iocn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      background-color: transparent;
+      color: white;
+      width: 150px;
+      font-size: 20px;
+      padding: 0 50px 0 50px;
 
-    img {
-      width: 30px;
-      border-radius: 50%;
+      img {
+        width: 30px;
+        border-radius: 50%;
+      }
     }
-  }
 
-  .font-iocn {
-    color: #cecece;
-  }
+    .font-iocn {
+      color: #cecece;
+    }
 
-  .lanmu {
-    display: flex;
-    height: 70px;
-    font-size: 20px;
-    color: white;
-    div {
-      //   margin: 0 20px;
-      width: 100px;
-      text-align: center;
+    .lanmu {
+      display: flex;
       height: 70px;
-      line-height: 70px;
-    }
-  }
+      font-size: 20px;
+      color: white;
 
-  .sousuo {
-    width: 250px;
-    background-color: rgb(228, 228, 228);
-    height: 30px;
-    border-radius: 10px;
-    position: relative;
-    .sousuo-icon {
-      font-size: 30px;
-      position: absolute;
-      top: 0;
-      left: 0px;
+      div {
+        //   margin: 0 20px;
+        width: 100px;
+        text-align: center;
+        height: 70px;
+        line-height: 70px;
+      }
     }
-    .sousuo-input {
-      position: absolute;
-      top: 0;
-      left: 40px;
-      height: calc(30px - 2px);
+
+    .lanmu-phone {
+      display: none;
+    }
+
+    .sousuo {
       width: 250px;
-      border: 1px solid rgb(228, 228, 228);
       background-color: rgb(228, 228, 228);
+      height: 30px;
+      border-radius: 10px;
+      position: relative;
+      .sousuo-icon {
+        font-size: 30px;
+        position: absolute;
+        top: 0;
+        left: 0px;
+      }
+      .sousuo-input {
+        position: absolute;
+        top: 0;
+        left: 40px;
+        height: calc(30px - 2px);
+        width: 250px;
+        border: 1px solid rgb(228, 228, 228);
+        background-color: rgb(228, 228, 228);
+      }
+    }
+
+    .my {
+      width: 50px;
+      height: 50px;
+      padding: 0 30px;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 25px;
+      }
     }
   }
+}
 
-  .my {
-    width: 50px;
-    height: 50px;
-    padding: 0 30px;
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 25px;
+// 中等屏幕
+@media (min-width: @breakpoint-small) and (max-width: @breakpoint-medium) {
+  .nav {
+    height: 70px;
+    width: 100%;
+    background-color: #333;
+    display: flex;
+    //   padding: 0 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    .music-iocn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      background-color: transparent;
+      color: white;
+      width: 150px;
+      font-size: 10px;
+      padding: 0 20px;
+
+      img {
+        width: 30px;
+        border-radius: 50%;
+      }
+    }
+
+    .font-iocn {
+      color: #cecece;
+    }
+
+    .lanmu {
+      display: flex;
+      height: 70px;
+      font-size: 15px;
+      color: white;
+
+      div {
+        //   margin: 0 20px;
+        width: 70px;
+        text-align: center;
+        height: 70px;
+        line-height: 70px;
+      }
+    }
+
+    .lanmu-phone {
+      display: none;
+    }
+
+    .sousuo {
+      width: 150px;
+      background-color: rgb(228, 228, 228);
+      height: 30px;
+      border-radius: 10px;
+      position: relative;
+      .sousuo-icon {
+        font-size: 30px;
+        position: absolute;
+        top: 0;
+        left: 0px;
+      }
+      .sousuo-input {
+        position: absolute;
+        top: 0;
+        left: 40px;
+        height: calc(30px - 2px);
+        width: 150px;
+        border: 1px solid rgb(228, 228, 228);
+        background-color: rgb(228, 228, 228);
+      }
+    }
+
+    .my {
+      width: 50px;
+      height: 50px;
+      padding: 0 30px;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 25px;
+      }
+    }
+  }
+}
+
+// 小屏幕
+@media (max-width: @breakpoint-small) {
+  .nav {
+    height: 70px;
+    width: 100%;
+    background-color: #333;
+    display: flex;
+    //   padding: 0 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    .music-iocn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      background-color: transparent;
+      color: white;
+      width: 20px;
+      font-size: 0px;
+      padding: 0 0 0 10px;
+
+      img {
+        width: 20px;
+        border-radius: 50%;
+      }
+    }
+
+    .font-iocn {
+      color: #cecece;
+    }
+
+    .lanmu {
+      display: flex;
+      height: 70px;
+      font-size: 10px;
+      color: white;
+      display: none;
+
+      div {
+        width: 50px;
+        text-align: center;
+        height: 70px;
+        line-height: 70px;
+        padding: 0 5px;
+      }
+    }
+
+    .lanmu-phone {
+      .lanmu-icon {
+        font-size: 30px;
+        color: #d1bfbf;
+      }
+    }
+
+    .sousuo {
+      width: 100px;
+      background-color: rgb(228, 228, 228);
+      height: 30px;
+      border-radius: 10px;
+      position: relative;
+      .sousuo-icon {
+        font-size: 30px;
+        position: absolute;
+        top: 0;
+        left: 0px;
+      }
+      .sousuo-input {
+        position: absolute;
+        top: 0;
+        left: 40px;
+        height: calc(30px - 2px);
+        width: 50px;
+        border: 1px solid rgb(228, 228, 228);
+        background-color: rgb(228, 228, 228);
+      }
+    }
+
+    .my {
+      width: 50px;
+      height: 50px;
+      padding: 0 30px;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 25px;
+      }
     }
   }
 }
