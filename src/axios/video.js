5c3c589d751a09eb483257d8324c40aba45b1aeb
黄玉锦 <18647770@qq.com>
2023-12-16 21:05:26 +0800

    整合懒加载

--- src/axios/video.js
+++ src/axios/video.js
@@ -1,3 +1,7 @@
 import requests from './axios'
 // 获取相关视频内容
 export const reqAlbumList = (id) => requests({ url: `/album?id=${id}`, method: 'get' });
+// 视频(搜索的是MV)详细
+export const reqVideoDetail=(id)=>requests({url:`/mv/detail?mvid=${id}`,method:'post'})
+// mv 视频播放地址
+export const reqMvVideo=(id)=>requests({url:`mv/url?id=${id}`,method:'post'})
